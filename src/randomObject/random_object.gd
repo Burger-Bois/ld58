3a9098e905326d0404205452e5c295b264f1cc4a
zoe-k <5705621+zoe-k@users.noreply.github.com>
2025-10-04 16:19:54 +0100

    added random shape generation

---
+++ src/randomObject/random_object.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+func _init() -> void:
+	
+	var random_points = create_deformed_polygon(randf_range(3,10), randf_range(20,60))
+	var polygon = Polygon2D.new()
+	var collision = CollisionPolygon2D.new()
+	polygon.set_polygon(random_points)
+	collision.set_polygon(random_points)
+	add_child(polygon)
+	add_child(collision)	
+	
+	
+func create_deformed_polygon(vertex_count : int, radius : float) -> PackedVector2Array:
+	var points : PackedVector2Array = []
+	var angle_increment : float = TAU / vertex_count
+	for i in vertex_count:
+		var point : Vector2 = Vector2(radius * sin(angle_increment * i), radius * cos(angle_increment * i))
+		point += point.direction_to(Vector2.ZERO) * radius * 0.25 * randf()
+		points.append(point)
+	return points
