a014e1e0fc8ef4e73fef2d1b387b505c96abb91f
zoe-k <5705621+zoe-k@users.noreply.github.com>
2025-10-06 14:58:58 +0100

    colour warning when oxygen is low

--- src/ui/oxygen_bar.gd
+++ src/ui/oxygen_bar.gd
@@ -2,6 +2,18 @@ class_name OxygenBar extends ProgressBar
 
 func _ready():
 	SignalBus.player_oxygen_changed.connect(update_value)
+	update_value(100)
+	
 	
 func update_value(new_value: int):
 	value = new_value
+	if (oxygen_low()):
+		self.get("theme_override_styles/background").border_color = Color(0.447, 0.11, 0.129, 1.0)
+	else:
+		self.get("theme_override_styles/background").border_color = Color(0.8, 0.8, 0.8)
+		
+	
+func oxygen_low() -> bool:
+	if (value/max_value) * 100 < 20:
+		return true
+	return false
