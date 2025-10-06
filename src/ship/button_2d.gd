3e42366ceb83feaa80f22392e0f52bf7f4997a03
John Stones <unknown@email.invalid>
2025-10-06 22:51:39 +0100

    feature: add some labels

--- src/ship/button_2d.gd
+++ src/ship/button_2d.gd
@@ -1,7 +1,12 @@
 class_name Button2D
-extends Node2D
+extends Area2D
 
 signal pressed()
+
+
+func _ready() -> void:
+	body_entered.connect(func(__): %Label.show())
+	body_exited.connect(func(__): %Label.hide())
 
 
 func press() -> void:
