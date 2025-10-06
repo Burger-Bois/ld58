3038e099b258db1061405840c62986cf757c260c
John Stones <unknown@email.invalid>
2025-10-06 16:47:37 +0100

    feature: add trade areas

---
+++ src/level/trade_area.gd
@@ -0,0 +1,45 @@
+class_name TradeArea
+extends Area2D
+
+@export
+var reward_scene: PackedScene
+
+@export
+var item_group: String
+@export
+var cost: int
+
+var _current_items: Array[Item] = []
+var _current_amount: int = 0: set=_set_current_amount
+
+
+func _ready() -> void:
+	body_entered.connect(collect_item)
+	body_exited.connect(lose_item)
+
+
+func collect_item(body: Node2D) -> void:
+	if body.is_in_group(item_group):
+		var item := body as Item
+		_current_items.append(item)
+		_current_amount += item.points
+		print('%s: %s' % [name, _current_amount])
+
+
+func lose_item(body: Node2D) -> void:
+	if body.is_in_group(item_group):
+		var item := body as Item
+		_current_items.erase(item)
+		_current_amount += -item.points
+		print('%s: %s' % [name, _current_amount])
+
+
+func _set_current_amount(new_amount: int) -> void:
+	_current_amount = new_amount
+	if _current_amount >= cost:
+		var reward := reward_scene.instantiate() as Node2D
+		reward.global_position = global_position
+		add_sibling(reward)
+		queue_free()
+		for item in _current_items:
+			item.queue_free()
